75f97124b6cc381b4953629c119c54dd793aefb4
zeynalnasibov17@mail.ru <zeynalnasibov17@mail.ru>
2022-05-19 15:11:00 +0400

    change css

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,8 +5,6 @@ import { Watchlist } from "./components/Watchlist";
 import { Watched } from "./components/Watched";
 import { Add } from "./components/Add";
 import "./App.css";
-import "./lib/font-awesome/css/all.min.css";
-
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
